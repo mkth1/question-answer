7553d623d67d43d21f87b01d7a68e092a0c7bd41
mukesh <pace.akash@gmail.com>
2014-09-25 12:38:43 +0300

    bootstrap added

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -40,51 +40,46 @@
 <body>
     <!-- Container -->
     <div class="container">
-        <!-- Navbar -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Laravel 4 Starter</a>
-          </div>
+    <!-- row 1: navigation -->
+	    <div class="row">
+	        <!-- the role navigation section is the navbar ,navbar-fixed-top is for full width , navbar-inverse is for color black or inverse color and icon-bar is the icons which comes on the button when u reduce the page size!-->
+	        <nav class="navbar navbar-default navbar-inverse navbar-fixed-top" role="navigation">
+	            <div class="navbar-header">
+	            	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse" >
+	                    <span class="src-only"></span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                </button>
+	                <a href="" class="navbar-brand">My Website</a>
+	            </div>
+	            <div class="collapse navbar-collapse" id="collapse">
+	                <ul class="nav navbar-nav">
+	                    <li><a href="#">Home</a></li>
+	                    <li><a href="#">About</a></li>
+	                    <li class="active"><a href="#">Services</a></li>
+	                    <li><a href="#">Photo Gallery</a></li>
+	                    <li><a href="#">Contact</a></li>
+	                </ul>
+	            </div>
+	        </nav>
+	    </div>
 
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse navbar-ex1-collapse">
-            <ul class="nav navbar-nav">
-              <li class="{{ (Request::is('/') ? ' active' : '') }}"><a href="/"><span class="glyphicon glyphicon-home"></span> Home</a></li>
-              <li><a href="{{{ URL::to('widgets') }}}">Widgets</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
-            </ul>
+	<!-- Content -->
+		@yield('content')
+	<!-- ./ content -->
 
-        <!-- Content -->
-        @yield('content')
-        <!-- ./ content -->
+	<!-- Footer -->
+	<footer class="clearfix">
+		@yield('footer')
+	</footer>
+	<!-- ./ Footer -->
 
-        <!-- Footer -->
-        <footer class="clearfix">
-            @yield('footer')
-        </footer>
-        <!-- ./ Footer -->
 
-    </div>
     <!-- ./ container -->
 
     <!-- Javascripts -->
-    {{ HTML::script('js/jquery-1.10.2.min.js') }}
+    {{ HTML::script('js/jquery-2.1.1.min.js') }}
     {{ HTML::script('js/bootstrap.min.js') }}
 
     @yield('scripts')
