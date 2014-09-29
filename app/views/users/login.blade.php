63818745e6cf9a88cecb9eab1de53d4c5f0793d1
mukesh <pace.akash@gmail.com>
2014-09-29 18:31:26 +0300

    makeover for user forms done

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -6,19 +6,29 @@
 @stop
 
 @section('content')
-	<h1>Login</h1>
-	{{ Form::open( array('url'=>'login') ) }}
+<legend class="text-center">Login</legend>
+
+	{{ Form::open( array('url'=>'login','class' =>'form-horizontal') ) }}
 	{{ Form::token() }}
-		<p>
-			{{ Form::label('username','Username') }} <br \>
-			{{ Form::text('username',Input::old('username')) }}
-		</p>
 
-		<p>
-			{{ Form::label('password','Password') }} <br \>
-			{{ Form::password('password') }}
-		</p>
+	<div class="form-group">
+		{{ Form::label('username','Username',['class'=>'col-md-2 control-label']) }}
+		<div class="col-md-8">
+			{{ Form::text('username',Input::old('username'),['class'=>'form-control'] ) }}
+		</div>
+	</div>
 
-		<p> {{ Form::submit('Login',['class' => 'btn btn-primary']) }} </p>
+	<div class="form-group">
+		{{ Form::label('password','Password',['class'=>'col-md-2 control-label']) }}
+		<div class="col-md-8">
+			{{ Form::password('password',['class'=>'form-control'] ) }}
+		</div>
+	</div>
+
+	<div class="form-group">
+		<div class="col-sm-offset-2 col-sm-10">
+			{{ Form::submit('Login',['class' => 'btn btn-primary']) }}
+		</div>
+	</div>
 	{{ Form::close() }}
 @stop
--- app/views/users/register.blade.php
+++ app/views/users/register.blade.php
@@ -7,32 +7,42 @@
 
 @section('content')
 	@if( $errors->has() )
-		<p>the errors are here</p>
-
-		<ul class="alert alert-danger">
+		<div class="alert alert-danger">
 			{{ $errors->first('username','<li>:message</li>')}}
 			{{ $errors->first('password','<li>:message</li>')}}
 			{{ $errors->first('password_confirmation','<li>:message</li>')}}
-		</ul>
+		</div>
 	@endif
 
-	{{ Form::open( array('url'=>'register')) }}
+	{{ Form::open( array('url'=>'register', 'class' =>'form-horizontal')) }}
+	<legend class="text-center">Register</legend>
 	{{ Form::token() }}
-		<p>
-			{{ Form::label('username','Username') }} <br />
-			{{ Form::text('username',Input::old('username') ) }}
-		</p>
 
-		<p>
-			{{ Form::label('password','Password') }} <br />
-			{{ Form::password('password') }}
-		</p>
+	<div class="form-group">
+		{{ Form::label('username','Username',['class'=>'col-md-2 control-label']) }}
+		<div class="col-md-8">
+			{{ Form::text('username',Input::old('username'),['class'=>'form-control'] ) }}
+		</div>
+	</div>
 
-		<p>
-			{{ Form::label('password_confirmation','Confirm Password') }} <br />
-			{{ Form::password('password_confirmation') }}
-		</p>
+	<div class="form-group">
+		{{ Form::label('password','Password',['class'=>'col-md-2 control-label']) }}
+		<div class="col-md-8">
+			{{ Form::password('password',['class'=>'form-control'] ) }}
+		</div>
+	</div>
 
-		<p> {{ Form::submit('Register',['class' => 'btn btn-primary']) }}</p>
+	<div class="form-group">
+		{{ Form::label('password_confirmation','Confirm',['class'=>'col-md-2 control-label']) }}
+		<div class="col-md-8">
+			{{ Form::password('password_confirmation',['class'=>'form-control'] ) }}
+		</div>
+	</div>
+
+	<div class="form-group">
+		<div class="col-sm-offset-2 col-sm-10">
+			{{ Form::submit('Register',['class' => 'btn btn-primary']) }}
+		</div>
+	</div>
 	{{ Form::close() }}
 @stop
